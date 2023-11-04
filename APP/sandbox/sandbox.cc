f24e772866240902110a0573dba6bae3c229d036
LJYC <ljyc.me@gmail.com>
2023-11-04 11:33:51 +0900

    Vcpkg has been integrated

--- APP/sandbox/sandbox.cc
+++ APP/sandbox/sandbox.cc
@@ -3,28 +3,39 @@
 #include <Albedo.Core.Log>
 #include <Albedo.Editor>
 
+#include <sstream>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/draw_surface_mesh.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_3 Point_3;
+typedef CGAL::Surface_mesh<Point_3> Mesh;
+
 namespace Albedo{
 namespace APP
 {
 	
 	void Sandbox()
 	{
-		// Write your codes here.
-		static bool init = false;
-		if (!init) Editor::RegisterUIEvent(new UIEvent{
-			"Sandbox", []()
-			{
-				ImGui::Begin("Hello");
-				{
-					if (ImGui::Button("Click me!"))
-					{
-						Log::Info("Clicked!");
-					}
-				}
-				ImGui::End();
-			} });
+		Mesh mesh;
 
-		init = true;
+		// Add 4 vertices
+		Mesh::Vertex_index v0 = mesh.add_vertex(Point_3(0,0,0));
+		Mesh::Vertex_index v1 = mesh.add_vertex(Point_3(1,0,0));
+		Mesh::Vertex_index v2 = mesh.add_vertex(Point_3(0,1,0));
+		Mesh::Vertex_index v3 = mesh.add_vertex(Point_3(0,0,1));
+
+		// Add 4 faces
+		mesh.add_face(v0,v1,v2);
+		mesh.add_face(v0,v1,v3);
+		mesh.add_face(v0,v2,v3);
+		mesh.add_face(v1,v2,v3);
+
+		// Draw the mesh
+		std::string s{ mesh };
+		s << mesh;
+		Log::Info("{}", s.str());
 	}
 
 }} // namespace Albedo::APP
